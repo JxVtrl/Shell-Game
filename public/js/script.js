2f8c6ef6068020d027367f7e52a4882f5c23c52b
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-11-12 03:55:42 -0300

    :sparkles: new Script

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,7 @@ const second_section = document.getElementById('second-section');
 const user_name_nav = document.getElementById('user-name-nav');
 
 var userName = ''
+var nivel = 1
 
 function checkName() {
     var name = nameBox.value
@@ -30,7 +31,6 @@ function checkName() {
 
            // create array of name
             var name_array = name.split("");
-            console.log(name_array);
            
            // check if name is valid
            name_array.map(function isLetter(character) {
@@ -64,12 +64,86 @@ function checkName() {
 const nivel_container = document.getElementById('nivel-container');
 const start_container = document.getElementById('start-container');
 const game_container = document.getElementById('game-container');
+const nivel_value = document.getElementById('nivel');
+
 
 function startGame() {
     nivel_container.classList.remove('hide');
     game_container.classList.remove('hide');
     start_container.classList.add('hide');
+    
 
+    // Get Level
+    nivel_value.innerHTML = formatNumber(nivel);
 
+    // Impress Game
+    for (let i = 0; i < 3; i++){
+        if (i == 1) {
+            game_container.innerHTML += `
+                <div class="cup-container">
+                    <img src="public/assets/cup1.png" alt="cup${i}" class="cup transparent">
+                    <img src="public/assets/ball${getRandomNumber(1, 4)}.png" alt="ball" class="ball" id="ball">
+                </div>
+            `
+        }
+        else {
+            game_container.innerHTML += `
+                <div class="cup-container">
+                    <img src="public/assets/cup1.png" alt="cup${i}" class="cup">
+                </div>
+            `
+            if (i == 2) {
+                const ball = document.getElementById('ball');
 
+                // Hide Ball
+                setTimeout(() => {
+                    document.querySelector('.cup.transparent').classList.remove('transparent');
+                    ball.classList.add('hide');
+
+                    randomizeCups()
+                }, 1200);
+            }
+        
+        }
+    }
+}
+
+function randomizeCups() {
+    const cups = document.querySelectorAll('.cup');
+
+    // add event listener to cups
+    cups.forEach(cup => {
+        cup.addEventListener('click', function checkBall() {
+            // See if ball is in cup
+            cup.classList.add('transparent');
+            setTimeout(() => {
+                cup.classList.remove('transparent');
+            }, 500);
+            
+            let cupFather = cup.parentElement;
+            console.log(cupFather)
+
+            let lengthFather = cupFather.childElementCount
+            console.log(lengthFather)
+
+            if(lengthFather == 2) {
+                ball.classList.remove('hide');
+                cup.classList.add('win');
+            }  
+
+        }, false)
+            
+    })
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function formatNumber(num) {
+    if(num < 10) {
+        return "0" + num;
+    }
+
+    return num
 }
